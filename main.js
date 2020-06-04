325310347d9716a53609983f34d045dc4901fa08
smailmalki <60870054+smailmalki@users.noreply.github.com>
2020-06-04 19:35:41 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const jquery = require('jquery')
-const { app, BrowserWindow } = require('electron')
+//const { app, BrowserWindow } = require('electron')
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
 function createWindow () {
   // Cree la fenetre du navigateur.
@@ -21,7 +24,41 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Certaines APIs peuvent être utilisées uniquement quand cet événement est émit.
-app.whenReady().then(createWindow)
+//app.whenReady().then(createWindow)
+global.mainWindow;
+
+app.on('ready', function() {
+  createWindow()
+  
+  var electronScreen = electron.screen;
+  var displays = electronScreen.getAllDisplays();
+  var externalDisplay = null;
+  for (var i in displays) {
+    if (displays[i].bounds.x != 0 || displays[i].bounds.y != 0) {
+      externalDisplay = displays[i];
+      break;
+    }
+  }
+
+
+  if (externalDisplay) {
+    mainWindow = new BrowserWindow({
+      x: externalDisplay.bounds.x + 50,
+      y: externalDisplay.bounds.y + 50,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    });
+    mainWindow.setFullScreen(true)
+    mainWindow.setMenu(null)
+    mainWindow.loadFile('deport.html')
+    /*var ipcMain = require('electron').ipcMain;
+    ipcMain.on('store-data', (event, payload) => {
+      mainWindow.webContents.send('store-data', payload);
+    });*/
+  }
+  
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -36,6 +73,6 @@ app.on('activate', () => {
   // Sur macOS, il est commun de re-créer une fenêtre de l'application quand
   // l'icône du dock est cliquée et qu'il n'y a pas d'autres fenêtres d'ouvertes.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    //createWindow()
   }
 })
